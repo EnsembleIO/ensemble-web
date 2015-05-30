019dac8dd9519ac0c7399f352d088ae5f4215d43
Jean Paliès <jean.palies@gmail.com>
2015-05-30 09:46:50 +0200

    Manque réseau et agenda
    Problèmes d'images non présentes et de données mal formatées

--- src/app/index.js
+++ src/app/index.js
@@ -1,7 +1,7 @@
 'use strict';
 
 angular.module('ensembleWeb', ['ngAnimate', 'ngCookies', 'ngTouch', 'ngSanitize', 'ngResource', 'ui.router', 'ngMaterial'])
-  .config(function ($stateProvider, $urlRouterProvider) {
+  .config(function ($stateProvider, $urlRouterProvider, $mdThemingProvider, $mdIconProvider) {
     $stateProvider
       .state('home', {
         url: '/',
@@ -10,5 +10,13 @@ angular.module('ensembleWeb', ['ngAnimate', 'ngCookies', 'ngTouch', 'ngSanitize'
       });
 
     $urlRouterProvider.otherwise('/');
+
+    $mdThemingProvider.theme('default');
+
+    $mdIconProvider
+    .iconSet('social', 'assets/images/icons/sets/social-icons.svg', 24)
+    .iconSet('device', 'assets/images/icons/sets/device-icons.svg', 24)
+    .iconSet('communication', 'assets/images/icons/sets/communication-icons.svg', 24)
+    .defaultIconSet('assets/images/icons/sets/core-icons.svg', 24);
   })
 ;
